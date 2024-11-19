8a38fc12cd35da43e347154f5b8d6c905c0db5be
Toby Palethorpe <tobypalethorpe@gmail.com>
2024-11-19 14:57:58 +0900

    added controller and show page

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,10 @@
 class BikesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
+  def index
+  end
+
+  def show
+    @bike = Bike.find(params[:id])
+  end
 end
