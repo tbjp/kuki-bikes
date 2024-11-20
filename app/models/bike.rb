0b8fdf86152994f2882ecef43f3eecd2637115ac
JarodMizgalski <jarodmiz2018@gmail.com>
2024-11-20 13:46:12 +0900

    started adding map

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -8,4 +8,6 @@ class Bike < ApplicationRecord
   validates :location, presence: true
   validates :description, presence: true
   validates :price_per_day, presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,5 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "flatpickr" # @4.6.13
 pin "flatpickr/dist/flatpickr.css", to: "flatpickr--dist--flatpickr.css.js" # @4.6.13
+pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.1.2/dist/mapbox-gl.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.1.0/nodelibs/browser/process-production.js"
