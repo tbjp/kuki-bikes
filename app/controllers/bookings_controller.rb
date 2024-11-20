916ae594cc89d670b10d956c79b68be2e68d517b
Toby Palethorpe <tobypalethorpe@gmail.com>
2024-11-20 15:05:36 +0900

    ready to test on heroku

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,8 +15,7 @@ class BookingsController < ApplicationController
   end
 
   def index
-
-    @bookings = current_user.bookings
+    @bookings = current_user.bookings.order(id: :desc)
   end
 
   private
