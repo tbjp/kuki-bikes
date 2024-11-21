0464d41906f51b103a057753bcb4e5bb70cb0a37
Bartek Mikorski <b.mikorski@gmail.com>
2024-11-21 13:06:41 +0900

    added a bunch of if statements checking if photo attached and created the add bike form

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -16,4 +16,23 @@ class BikesController < ApplicationController
     @bike = Bike.find(params[:id])
     @booking = Booking.new
   end
+
+  def new
+    @bike = Bike.new
+  end
+  def create
+    @bike = Bike.new(bike_params)
+    @bike.user = current_user
+    if @bike.save
+      redirect_to bike_path(@bike)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:brand, :model, :year, :location, :description, :price_per_day, :photo)
+  end
 end
