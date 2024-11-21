16cb68afe4abd106127f26e45b7bbe35df02f195
JarodMizgalski <jarodmiz2018@gmail.com>
2024-11-21 13:41:48 +0900

    adding approve button

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -4,4 +4,15 @@ class Owner::BookingsController < ApplicationController
     @owner_bookings = current_user.bookings_as_owner
     @bikes = current_user.bikes
   end
+
+  def update
+    @owner_bookings = current_user.bookings_as_owner.find(params[:id])
+    @owner_bookings.update(owner_bookings_params)
+  end
+
+  private
+
+  def owner_bookings_params
+    params.require(:booking).permit(:status)
+  end
 end
